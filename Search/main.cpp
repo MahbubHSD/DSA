073282469b5499b87012d66e0596f8af61080132
Md. Mahbub Hasan Shad <299721779+MahbubHSD@users.noreply.github.com>
2026-09-12 14:30:26 +0000

    Refactor main function in Search to include additional type aliases and a solve function

--- Search/main.cpp
+++ Search/main.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+void solve() {
+    printf("Do nothing\n");
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int t = 5;
+    // cin >> t;
+    while (t--) solve();
     return 0;
 }
